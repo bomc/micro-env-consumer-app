48e9cf0c6da718bafa828c0f82a9bb0361deee78
bomc <michael_boerner@t-online.de>
2021-07-29 17:53:35 +0200

    set version to v0.0.1

---
+++ run_image_build_push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo set docker daemon environment variables.
+eval $(minikube docker-env)
+
+echo run command 'docker build -t localhost:5000/bomc/consumer:v0.0.1'
+
+docker build -t localhost:5000/bomc/consumer:v0.0.1 .
